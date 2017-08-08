7578b82fbc028346041bac52524a3a0fb7956f00
Elrey Belmonti <elbelmonti@gmail.com>
2017-08-07 17:33:25 -0700

    WIP make the first letter of string uppercase

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 var sentence = prompt("Write a sentence: ");
 
 alert(sentence);
+
+var capitalizeFirstLast = function(sentence){
+  var firstLetterChange = 
+  sentence[0] = sentence.toUpperCase().charAt(0);
+  sentence[-1] = sentence.toUpperCase().charAt(-1);
+  return sentence;
+}
+
+alert(capitalizeFirstLast(sentence));
