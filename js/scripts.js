035ff1889d166e557f6ad3ccdacb09d5a75057be
ElreyB <elbelmonti@gmail.com>
2017-08-07 23:02:57 -0700

    uppercase first and last letter of a string

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,9 @@ var sentence = prompt("Write a sentence: ");
 alert(sentence);
 
 var capitalizeFirstLast = function(sentence){
-  var firstLetterChange = 
-  sentence[0] = sentence.toUpperCase().charAt(0);
-  sentence[-1] = sentence.toUpperCase().charAt(-1);
-  return sentence;
+  var removeFirstLetter = sentence.substring(1, sentence.length-1)
+  var newSentence = sentence[0].toUpperCase() + removeFirstLetter + sentence[sentence.length-1].toUpperCase();
+  return newSentence;
 }
 
 alert(capitalizeFirstLast(sentence));
