e4be2609125498e27a2993b91fc59b899134fe82
ElreyB <elbelmonti@gmail.com>
2017-08-07 23:18:01 -0700

    reverse first and last letter

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,17 @@ var sentence = prompt("Write a sentence: ");
 alert(sentence);
 
 var capitalizeFirstLast = function(sentence){
-  var removeFirstLetter = sentence.substring(1, sentence.length-1)
-  var newSentence = sentence[0].toUpperCase() + removeFirstLetter + sentence[sentence.length-1].toUpperCase();
+  var removeLetter = sentence.substring(1, sentence.length-1)
+  var newSentence = sentence[0].toUpperCase() + removeLetter + sentence[sentence.length-1].toUpperCase();
   return newSentence;
 }
 
 alert(capitalizeFirstLast(sentence));
+
+var reverseLetters = function(sentence){
+  var removeLetters = sentence.substring(1, sentence.length-1)
+  var newSentence = sentence[sentence.length-1] + removeLetters + sentence[0];
+  return newSentence;
+}
+
+alert(reverseLetters(sentence));
